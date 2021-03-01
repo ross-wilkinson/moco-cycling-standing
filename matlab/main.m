5dfa25c4cb63f58af00bd34771286ef324f31964
ross-wilkinson <ross.wilkinson@icloud.com>
2021-03-01 07:45:19 -0500

    create main.m

---
+++ matlab/main.m
@@ -0,0 +1,13 @@
+% MocoInverse pipeline for solving control and actuator states during
+% standing cycling
+
+% Author(s): Ross Wilkinson
+% Affiliation(s): Ross Wilkinson
+
+% GitHub Repo: https://github.com/ross-wilkinson/moco-cycling-standing
+
+%% Load experimental kinematics and external pedal forces
+
+%% Solve handlebar forces
+
+%% Solve 
